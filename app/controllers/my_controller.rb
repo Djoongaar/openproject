f58c399255ed31b84b69423ebeb84910655f34aa
Andreas Pfohl <a.pfohl@openproject.com>
2024-06-07 11:20:39 +0200

    [48619] Added naming of new API tokens

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -31,7 +31,7 @@ class MyController < ApplicationController
   include Accounts::UserPasswordChange
   include ActionView::Helpers::TagHelper
 
-  layout 'my'
+  layout "my"
 
   before_action :require_login
   before_action :set_current_user
@@ -39,12 +39,12 @@ class MyController < ApplicationController
   before_action :set_grouped_ical_tokens, only: %i[access_token]
   before_action :set_ical_token, only: %i[revoke_ical_token]
 
-  menu_item :account,             only: [:account]
-  menu_item :settings,            only: [:settings]
-  menu_item :password,            only: [:password]
-  menu_item :access_token,        only: [:access_token]
-  menu_item :notifications,       only: [:notifications]
-  menu_item :reminders,           only: [:reminders]
+  menu_item :account, only: [:account]
+  menu_item :settings, only: [:settings]
+  menu_item :password, only: [:password]
+  menu_item :access_token, only: [:access_token]
+  menu_item :notifications, only: [:notifications]
+  menu_item :reminders, only: [:reminders]
 
   def account; end
 
@@ -52,7 +52,7 @@ class MyController < ApplicationController
     write_settings
 
     # If mail changed, expire all other sessions
-    if @user.previous_changes['mail'] && ::Sessions::DropOtherSessionsService.call(@user, session)
+    if @user.previous_changes["mail"] && ::Sessions::DropOtherSessionsService.call(@user, session)
       flash[:info] = "#{flash[:notice]} #{t(:notice_account_other_session_expired)}"
       flash.delete :notice
     end
@@ -73,7 +73,7 @@ class MyController < ApplicationController
   # When making changes here, also check AccountController.change_password
   def change_password
     change_password_flow(user: @user, params:, update_legacy: false) do
-      redirect_to action: 'password'
+      redirect_to action: "password"
     end
   end
 
@@ -82,37 +82,37 @@ class MyController < ApplicationController
     @storage_tokens = OAuthClientToken
                         .preload(:oauth_client)
                         .joins(:oauth_client)
-                        .where(user: @user, oauth_client: { integration_type: 'Storages::Storage' })
+                        .where(user: @user, oauth_client: { integration_type: "Storages::Storage" })
   end
 
   def delete_storage_token
     token = OAuthClientToken
-      .preload(:oauth_client)
-      .joins(:oauth_client)
-      .where(user: @user, oauth_client: { integration_type: 'Storages::Storage' }).find_by(id: params[:id])
+              .preload(:oauth_client)
+              .joins(:oauth_client)
+              .where(user: @user, oauth_client: { integration_type: "Storages::Storage" }).find_by(id: params[:id])
 
     if token&.destroy
-      flash[:info] = I18n.t('my_account.access_tokens.storages.removed')
+      flash[:info] = I18n.t("my_account.access_tokens.storages.removed")
     else
-      flash[:error] = I18n.t('my_account.access_tokens.storages.failed')
+      flash[:error] = I18n.t("my_account.access_tokens.storages.failed")
     end
     redirect_to action: :access_token
   end
 
   # Configure user's in app notifications
   def notifications
-    render html: '',
-           layout: 'angular/angular',
+    render html: "",
+           layout: "angular/angular",
            locals: {
              menu_name: :my_menu,
-             page_title: [I18n.t(:label_my_account), I18n.t('js.notifications.settings.title')]
+             page_title: [I18n.t(:label_my_account), I18n.t("js.notifications.settings.title")]
            }
   end
 
   # Configure user's mail reminders
   def reminders
-    render html: '',
-           layout: 'angular/angular',
+    render html: "",
+           layout: "angular/angular",
            locals: { menu_name: :my_menu }
   end
 
@@ -120,50 +120,50 @@ class MyController < ApplicationController
   def generate_rss_key
     token = Token::RSS.create!(user: current_user)
     flash[:info] = [
-      t('my.access_token.notice_reset_token', type: 'RSS').html_safe,
+      t("my.access_token.notice_reset_token", type: "RSS").html_safe,
       content_tag(:strong, token.plain_value),
-      t('my.access_token.token_value_warning')
+      t("my.access_token.token_value_warning")
     ]
   rescue StandardError => e
     Rails.logger.error "Failed to reset user ##{current_user.id} RSS key: #{e}"
-    flash[:error] = t('my.access_token.failed_to_reset_token', error: e.message)
+    flash[:error] = t("my.access_token.failed_to_reset_token", error: e.message)
   ensure
-    redirect_to action: 'access_token'
+    redirect_to action: "access_token"
   end
 
   def revoke_rss_key
     current_user.rss_token.destroy
-    flash[:info] = t('my.access_token.notice_rss_token_revoked')
+    flash[:info] = t("my.access_token.notice_rss_token_revoked")
   rescue StandardError => e
     Rails.logger.error "Failed to revoke rss token ##{current_user.id}: #{e}"
-    flash[:error] = t('my.access_token.failed_to_reset_token', error: e.message)
+    flash[:error] = t("my.access_token.failed_to_reset_token", error: e.message)
   ensure
-    redirect_to action: 'access_token'
+    redirect_to action: "access_token"
   end
 
   # Create a new API key
   def generate_api_key
-    token = Token::API.create!(user: current_user)
+    token = Token::API.create!(user: current_user, data: { name: params[:token_name] })
     flash[:info] = [
-      t('my.access_token.notice_reset_token', type: 'API').html_safe,
+      t("my.access_token.notice_reset_token", type: "API").html_safe,
       content_tag(:strong, token.plain_value),
-      t('my.access_token.token_value_warning')
+      t("my.access_token.token_value_warning")
     ]
   rescue StandardError => e
     Rails.logger.error "Failed to reset user ##{current_user.id} API key: #{e}"
-    flash[:error] = t('my.access_token.failed_to_reset_token', error: e.message)
+    flash[:error] = t("my.access_token.failed_to_reset_token", error: e.message)
   ensure
-    redirect_to action: 'access_token'
+    redirect_to action: "access_token"
   end
 
   def revoke_api_key
     current_user.api_tokens.find(params[:token_id]).destroy
-    flash[:info] = t('my.access_token.notice_api_token_revoked')
+    flash[:info] = t("my.access_token.notice_api_token_revoked")
   rescue StandardError => e
     Rails.logger.error "Failed to revoke api token ##{current_user.id}: #{e}"
-    flash[:error] = t('my.access_token.failed_to_reset_token', error: e.message)
+    flash[:error] = t("my.access_token.failed_to_reset_token", error: e.message)
   ensure
-    redirect_to action: 'access_token'
+    redirect_to action: "access_token"
   end
 
   def revoke_ical_token
@@ -172,9 +172,9 @@ class MyController < ApplicationController
     flash[:info] = message
   rescue StandardError => e
     Rails.logger.error "Failed to revoke all ical tokens for ##{current_user.id}: #{e}"
-    flash[:error] = t('my.access_token.failed_to_reset_token', error: e.message)
+    flash[:error] = t("my.access_token.failed_to_reset_token", error: e.message)
   ensure
-    redirect_to action: 'access_token'
+    redirect_to action: "access_token"
   end
 
   def default_breadcrumb
@@ -190,7 +190,7 @@ class MyController < ApplicationController
   def redirect_if_password_change_not_allowed_for(user)
     unless user.change_password_allowed?
       flash[:error] = I18n.t(:notice_can_t_change_password)
-      redirect_to action: 'account'
+      redirect_to action: "account"
       return true
     end
     false
@@ -198,8 +198,8 @@ class MyController < ApplicationController
 
   def write_settings
     result = Users::UpdateService
-             .new(user: current_user, model: current_user)
-             .call(user_params)
+               .new(user: current_user, model: current_user)
+               .call(user_params)
 
     if result&.success
       flash[:notice] = notice_account_updated
@@ -227,7 +227,7 @@ class MyController < ApplicationController
   end
 
   def error_account_update_failed(result)
-    errors = result ? result.errors.full_messages.join("\n") : ''
+    errors = result ? result.errors.full_messages.join("\n") : ""
     [t(:notice_account_update_failed), errors]
   end
 
@@ -245,14 +245,14 @@ class MyController < ApplicationController
 
   def set_grouped_ical_tokens
     @ical_tokens_grouped_by_query = current_user.ical_tokens
-      .joins(ical_token_query_assignment: { query: :project })
-      .select("tokens.*, ical_token_query_assignments.query_id")
-      .group_by(&:query_id)
+                                                .joins(ical_token_query_assignment: { query: :project })
+                                                .select("tokens.*, ical_token_query_assignments.query_id")
+                                                .group_by(&:query_id)
   end
 
   def ical_destroy_info_message
     t(
-      'my.access_token.notice_ical_token_revoked',
+      "my.access_token.notice_ical_token_revoked",
       token_name: @ical_token.ical_token_query_assignment.name,
       calendar_name: @ical_token.query.name,
       project_name: @ical_token.query.project.name
